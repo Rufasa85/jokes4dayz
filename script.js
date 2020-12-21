c30084d4f02d69efdc69888f67bed9ceab955ed7
Denis Molloy <denis.molloy@gmail.com>
2020-12-21 10:59:39 -0800

    successfully fetching joke

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('linked!')
+fetch('https://icanhazdadjoke.com/', {
+  headers: {
+    'Accept': 'application/json'
+  }
+})
+  .then(function (res) {
+    return res.json()
+  })
+  .then(function (data) {
+    console.log(data)
+  }).catch(function (err) {
+    console.log(err)
+  })
